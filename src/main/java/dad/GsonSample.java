6253beee4b56bb0492cf7c98ebb11ba9d296ac3b
MichelReyes <michelreyesperez29@gmail.com>
2021-10-11 15:40:37 +0100

    Initial commit

---
+++ src/main/java/dad/GsonSample.java
@@ -0,0 +1,44 @@
+package dad;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonSample {
+
+	public static void main(String[] args) {
+		
+		String nombre, apellidos;
+		int edad=0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Dame un nombre: ");
+		nombre=scanner.nextLine();
+		while(nombre.isBlank()) {
+			System.out.println("Por favor, dame un nombre: ");
+			nombre=scanner.nextLine();
+		}
+		
+		System.out.println("Dame un apellido: ");
+		apellidos=scanner.nextLine();
+		while(apellidos.isBlank()) {
+			System.out.println("Por favor, dame un apellido: ");
+			apellidos=scanner.nextLine();
+		}
+		
+		System.out.println("Por último, dime la edad: ");
+		try{
+			edad=scanner.nextInt();
+		}catch(InputMismatchException e) {
+			System.out.println("Eso no es un numero! Asegurate de introducir un numero: ");
+		}	
+		Persona p = new Persona(nombre, apellidos, edad);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(p);
+		System.out.println(json);
+	}
+
+}
